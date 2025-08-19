c578f599b0366f80c4c44ce86a7c2af4cd8154d5
Thomas Hagström <thomashagstrom@outlook.com>
2025-08-19 14:36:39 +0200

    iOS native module should match spec

--- specs/index.ts
+++ specs/index.ts
@@ -1,6 +1,6 @@
 // Reexport the native module. On web, it will be resolved to ThermalibExpoModule.web.ts
 // and on native platforms to ThermalibExpoModule.ts
-import mod from './NativeThermaLibSpec';
+import mod from './NativeThermalibReactNativeSpec';
 export {mod as thermalib};
 export type {Device} from './types/';
 export * from './requestBluetoothPermission';
